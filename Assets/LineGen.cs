26696cb7b3275c3525cba69c5e84c931c8a1d629
Sora <marcaceedu@gmail.com>
2026-03-10 21:58:08 +0800

    Week 10 Activity
    week 10 activity

--- Assets/LineGen.cs
+++ Assets/LineGen.cs
@@ -9,19 +9,35 @@ public class LineGen : MonoBehaviour
     public float sphereRadius = 1.0f;
     [Range(4, 32)] public int sphereLatitudes = 8;  
     [Range(4, 32)] public int sphereLongitudes = 12; 
+    public Vector3 sphereRot;
+    public Vector3 sphereRotSpeed;
 
     [Header("Cube Settings")]
     public Vector3 cubePos;
     public float cubeSize = 1.0f;
+    public Vector3 cubeRot;
+    public Vector3 cubeRotSpeed;
 
     [Header("Pyramid Settings")]
     public Vector3 pyramidPos;
     public float pyramidSize = 1.0f;
+    public Vector3 pyramidRot;
+    public Vector3 pyramidRotSpeed;
 
     [Header("Capsule Settings")]
     public Vector3 capsulePos;
     public float capsuleRadius = 0.5f;
     public float capsuleHeight = 2.0f;
+    public Vector3 capsuleRot;
+    public Vector3 capsuleRotSpeed;
+
+    private void Update()
+    {
+        sphereRot += sphereRotSpeed * Time.deltaTime;
+        cubeRot += cubeRotSpeed * Time.deltaTime;
+        pyramidRot += pyramidRotSpeed * Time.deltaTime;
+        capsuleRot += capsuleRotSpeed * Time.deltaTime;
+    }
 
     private void OnPostRender()
     {
@@ -31,10 +47,10 @@ public class LineGen : MonoBehaviour
         GL.PushMatrix();
         GL.Begin(GL.LINES);
 
-        DrawWireSphere(spherePos, sphereRadius, sphereLatitudes, sphereLongitudes);
-        DrawWireCube(cubePos, cubeSize);
-        DrawWirePyramid(pyramidPos, pyramidSize);
-        DrawWireCapsule(capsulePos, capsuleRadius, capsuleHeight, 16);
+        DrawWireSphere(spherePos, sphereRadius, sphereLatitudes, sphereLongitudes, sphereRot);
+        DrawWireCube(cubePos, cubeSize, cubeRot);
+        DrawWirePyramid(pyramidPos, pyramidSize, pyramidRot);
+        DrawWireCapsule(capsulePos, capsuleRadius, capsuleHeight, 16, capsuleRot);
 
         GL.End();
         GL.PopMatrix();
@@ -43,7 +59,7 @@ public class LineGen : MonoBehaviour
     #region Sphere Logic
 
 
-    private void DrawWireSphere(Vector3 center, float radius, int lats, int longs)
+    private void DrawWireSphere(Vector3 center, float radius, int lats, int longs, Vector3 rotation)
     {
         for (int i = 1; i < lats; i++)
         {
@@ -51,7 +67,7 @@ public class LineGen : MonoBehaviour
             float y = Mathf.Cos(phi) * radius;
             float ringRadius = Mathf.Sin(phi) * radius;
 
-            DrawCircleXZ(center + new Vector3(0, y, 0), ringRadius, longs * 2);
+            DrawCircleXZ(center, ringRadius, y, longs * 2, rotation);
         }
 
         for (int i = 0; i < longs; i++)
@@ -68,19 +84,22 @@ public class LineGen : MonoBehaviour
                 Vector3 p1 = new Vector3(Mathf.Sin(phi1) * cosTheta, Mathf.Cos(phi1), Mathf.Sin(phi1) * sinTheta) * radius;
                 Vector3 p2 = new Vector3(Mathf.Sin(phi2) * cosTheta, Mathf.Cos(phi2), Mathf.Sin(phi2) * sinTheta) * radius;
 
-                DrawLine3D(center + p1, center + p2);
+                DrawLine3D(center + RotatePoint(p1, rotation), center + RotatePoint(p2, rotation));
             }
         }
     }
 
-    private void DrawCircleXZ(Vector3 center, float radius, int segments)
+    private void DrawCircleXZ(Vector3 center, float radius, float yOffset, int segments, Vector3 rotation)
     {
         for (int i = 0; i < segments; i++)
         {
             float a1 = (float)i / segments * Mathf.PI * 2;
             float a2 = (float)(i + 1) / segments * Mathf.PI * 2;
-            DrawLine3D(center + new Vector3(Mathf.Cos(a1) * radius, 0, Mathf.Sin(a1) * radius),
-                       center + new Vector3(Mathf.Cos(a2) * radius, 0, Mathf.Sin(a2) * radius));
+
+            Vector3 p1 = new Vector3(Mathf.Cos(a1) * radius, yOffset, Mathf.Sin(a1) * radius);
+            Vector3 p2 = new Vector3(Mathf.Cos(a2) * radius, yOffset, Mathf.Sin(a2) * radius);
+
+            DrawLine3D(center + RotatePoint(p1, rotation), center + RotatePoint(p2, rotation));
         }
     }
 
@@ -88,58 +107,99 @@ public class LineGen : MonoBehaviour
 
     #region Other Shapes Logic
 
-    private void DrawWireCube(Vector3 center, float size)
+    private void DrawWireCube(Vector3 center, float size, Vector3 rotation)
     {
         float h = size * 0.5f;
         Vector3[] v = {
-            new Vector3(center.x-h, center.y-h, center.z-h), new Vector3(center.x+h, center.y-h, center.z-h),
-            new Vector3(center.x+h, center.y+h, center.z-h), new Vector3(center.x-h, center.y+h, center.z-h),
-            new Vector3(center.x-h, center.y-h, center.z+h), new Vector3(center.x+h, center.y-h, center.z+h),
-            new Vector3(center.x+h, center.y+h, center.z+h), new Vector3(center.x-h, center.y+h, center.z+h)
+            RotatePoint(new Vector3(-h, -h, -h), rotation), RotatePoint(new Vector3(h, -h, -h), rotation),
+            RotatePoint(new Vector3(h, h, -h), rotation), RotatePoint(new Vector3(-h, h, -h), rotation),
+            RotatePoint(new Vector3(-h, -h, h), rotation), RotatePoint(new Vector3(h, -h, h), rotation),
+            RotatePoint(new Vector3(h, h, h), rotation), RotatePoint(new Vector3(-h, h, h), rotation)
         };
-        DrawLine3D(v[0], v[1]); DrawLine3D(v[1], v[2]); DrawLine3D(v[2], v[3]); DrawLine3D(v[3], v[0]);
-        DrawLine3D(v[4], v[5]); DrawLine3D(v[5], v[6]); DrawLine3D(v[6], v[7]); DrawLine3D(v[7], v[4]);
-        DrawLine3D(v[0], v[4]); DrawLine3D(v[1], v[5]); DrawLine3D(v[2], v[6]); DrawLine3D(v[3], v[7]);
+        DrawLine3D(center + v[0], center + v[1]); DrawLine3D(center + v[1], center + v[2]); DrawLine3D(center + v[2], center + v[3]); DrawLine3D(center + v[3], center + v[0]);
+        DrawLine3D(center + v[4], center + v[5]); DrawLine3D(center + v[5], center + v[6]); DrawLine3D(center + v[6], center + v[7]); DrawLine3D(center + v[7], center + v[4]);
+        DrawLine3D(center + v[0], center + v[4]); DrawLine3D(center + v[1], center + v[5]); DrawLine3D(center + v[2], center + v[6]); DrawLine3D(center + v[3], center + v[7]);
     }
 
 
-    private void DrawWirePyramid(Vector3 pos, float size)
+    private void DrawWirePyramid(Vector3 pos, float size, Vector3 rotation)
     {
         float h = size * 0.5f;
-        Vector3 apex = pos + Vector3.up * h;
-        Vector3[] b = { pos + new Vector3(-h, -h, -h), pos + new Vector3(h, -h, -h), pos + new Vector3(h, -h, h), pos + new Vector3(-h, -h, h) };
+        Vector3 apex = RotatePoint(Vector3.up * h, rotation);
+        Vector3[] b = { 
+            RotatePoint(new Vector3(-h, -h, -h), rotation), 
+            RotatePoint(new Vector3(h, -h, -h), rotation), 
+            RotatePoint(new Vector3(h, -h, h), rotation), 
+            RotatePoint(new Vector3(-h, -h, h), rotation) 
+        };
         for (int i = 0; i < 4; i++)
         {
-            DrawLine3D(b[i], b[(i + 1) % 4]);
-            DrawLine3D(b[i], apex);
+            DrawLine3D(pos + b[i], pos + b[(i + 1) % 4]);
+            DrawLine3D(pos + b[i], pos + apex);
         }
     }
 
 
-    private void DrawWireCapsule(Vector3 pos, float radius, float height, int segments)
+    private void DrawWireCapsule(Vector3 pos, float radius, float height, int segments, Vector3 rotation)
     {
         float cylH = Mathf.Max(0, height - (radius * 2));
-        Vector3 top = pos + Vector3.up * (cylH * 0.5f);
-        Vector3 bot = pos - Vector3.up * (cylH * 0.5f);
+        Vector3 topOffset = Vector3.up * (cylH * 0.5f);
+        Vector3 botOffset = -Vector3.up * (cylH * 0.5f);
 
-        DrawLine3D(top + Vector3.left * radius, bot + Vector3.left * radius);
-        DrawLine3D(top + Vector3.right * radius, bot + Vector3.right * radius);
-        DrawLine3D(top + Vector3.forward * radius, bot + Vector3.forward * radius);
-        DrawLine3D(top + Vector3.back * radius, bot + Vector3.back * radius);
+        DrawLine3D(pos + RotatePoint(topOffset + Vector3.left * radius, rotation), pos + RotatePoint(botOffset + Vector3.left * radius, rotation));
+        DrawLine3D(pos + RotatePoint(topOffset + Vector3.right * radius, rotation), pos + RotatePoint(botOffset + Vector3.right * radius, rotation));
+        DrawLine3D(pos + RotatePoint(topOffset + Vector3.forward * radius, rotation), pos + RotatePoint(botOffset + Vector3.forward * radius, rotation));
+        DrawLine3D(pos + RotatePoint(topOffset + Vector3.back * radius, rotation), pos + RotatePoint(botOffset + Vector3.back * radius, rotation));
 
         for (int i = 0; i < segments; i++)
         {
             float a1 = (float)i / segments * Mathf.PI;
             float a2 = (float)(i + 1) / segments * Mathf.PI;
 
-            DrawLine3D(top + new Vector3(Mathf.Cos(a1) * radius, Mathf.Sin(a1) * radius, 0), top + new Vector3(Mathf.Cos(a2) * radius, Mathf.Sin(a2) * radius, 0));
-            DrawLine3D(top + new Vector3(0, Mathf.Sin(a1) * radius, Mathf.Cos(a1) * radius), top + new Vector3(0, Mathf.Sin(a2) * radius, Mathf.Cos(a2) * radius));
+            Vector3 pTop1 = topOffset + new Vector3(Mathf.Cos(a1) * radius, Mathf.Sin(a1) * radius, 0);
+            Vector3 pTop2 = topOffset + new Vector3(Mathf.Cos(a2) * radius, Mathf.Sin(a2) * radius, 0);
+            DrawLine3D(pos + RotatePoint(pTop1, rotation), pos + RotatePoint(pTop2, rotation));
 
-            DrawLine3D(bot + new Vector3(Mathf.Cos(a1 + Mathf.PI) * radius, Mathf.Sin(a1 + Mathf.PI) * radius, 0), bot + new Vector3(Mathf.Cos(a2 + Mathf.PI) * radius, Mathf.Sin(a2 + Mathf.PI) * radius, 0));
-            DrawLine3D(bot + new Vector3(0, Mathf.Sin(a1 + Mathf.PI) * radius, Mathf.Cos(a1 + Mathf.PI) * radius), bot + new Vector3(0, Mathf.Sin(a2 + Mathf.PI) * radius, Mathf.Cos(a2 + Mathf.PI) * radius));
+            Vector3 pTop3 = topOffset + new Vector3(0, Mathf.Sin(a1) * radius, Mathf.Cos(a1) * radius);
+            Vector3 pTop4 = topOffset + new Vector3(0, Mathf.Sin(a2) * radius, Mathf.Cos(a2) * radius);
+            DrawLine3D(pos + RotatePoint(pTop3, rotation), pos + RotatePoint(pTop4, rotation));
+
+            Vector3 pBot1 = botOffset + new Vector3(Mathf.Cos(a1 + Mathf.PI) * radius, Mathf.Sin(a1 + Mathf.PI) * radius, 0);
+            Vector3 pBot2 = botOffset + new Vector3(Mathf.Cos(a2 + Mathf.PI) * radius, Mathf.Sin(a2 + Mathf.PI) * radius, 0);
+            DrawLine3D(pos + RotatePoint(pBot1, rotation), pos + RotatePoint(pBot2, rotation));
+
+            Vector3 pBot3 = botOffset + new Vector3(0, Mathf.Sin(a1 + Mathf.PI) * radius, Mathf.Cos(a1 + Mathf.PI) * radius);
+            Vector3 pBot4 = botOffset + new Vector3(0, Mathf.Sin(a2 + Mathf.PI) * radius, Mathf.Cos(a2 + Mathf.PI) * radius);
+            DrawLine3D(pos + RotatePoint(pBot3, rotation), pos + RotatePoint(pBot4, rotation));
         }
-        DrawCircleXZ(top, radius, segments);
-        DrawCircleXZ(bot, radius, segments);
+        DrawCircleXZ(pos, radius, topOffset.y, segments, rotation);
+        DrawCircleXZ(pos, radius, botOffset.y, segments, rotation);
+    }
+
+    private Vector3 RotatePoint(Vector3 p, Vector3 rotation)
+    {
+        float radX = rotation.x * Mathf.Deg2Rad;
+        float sinX = Mathf.Sin(radX);
+        float cosX = Mathf.Cos(radX);
+        float py = p.y * cosX - p.z * sinX;
+        float pz = p.y * sinX + p.z * cosX;
+        p.y = py; p.z = pz;
+
+        float radY = rotation.y * Mathf.Deg2Rad;
+        float sinY = Mathf.Sin(radY);
+        float cosY = Mathf.Cos(radY);
+        float px = p.x * cosY + p.z * sinY;
+        pz = -p.x * sinY + p.z * cosY;
+        p.x = px; p.z = pz;
+
+        float radZ = rotation.z * Mathf.Deg2Rad;
+        float sinZ = Mathf.Sin(radZ);
+        float cosZ = Mathf.Cos(radZ);
+        px = p.x * cosZ - p.y * sinZ;
+        py = p.x * sinZ + p.y * cosZ;
+        p.x = px; p.y = py;
+
+        return p;
     }
 
     private void DrawLine3D(Vector3 p1, Vector3 p2)
--- Assets/PyramidGenerator.cs
+++ Assets/PyramidGenerator.cs
@@ -5,10 +5,18 @@ public class PyramidGenerator : MonoBehaviour
 {
     public float baseLength = 1f;
     public float height = 1f;
+    public float rotSpeedX = 0f;
+    public float rotSpeedY = 0f;
+    public float rotSpeedZ = 0f;
 
     void Start()
     {
         GeneratePyramid();
+    }
+
+    void Update()
+    {
+        transform.Rotate(rotSpeedX * Time.deltaTime, rotSpeedY * Time.deltaTime, rotSpeedZ * Time.deltaTime);
     }
 
     void GeneratePyramid()
@@ -16,38 +24,30 @@ public class PyramidGenerator : MonoBehaviour
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        // Define the vertices of the pyramid
-        // A pyramid has a polygonal base and a common apex
         Vector3[] vertices = new Vector3[5];
         float offset = baseLength * 0.5f;
 
-        // Base vertices (at y=0)
         vertices[0] = new Vector3(-offset, 0, -offset);
         vertices[1] = new Vector3(offset, 0, -offset);
         vertices[2] = new Vector3(offset, 0, offset);
         vertices[3] = new Vector3(-offset, 0, offset);
-        // Apex vertex (at desired height)
         vertices[4] = new Vector3(0, height, 0);
 
         mesh.vertices = vertices;
 
-        // Define the triangles (connecting vertices to form faces)
         int[] triangles = new int[]
         {
-            // Base (two triangles)
-            0, 2, 1, // Bottom-left, top-right, bottom-right
-            0, 3, 2, // Bottom-left, top-left, top-right
+            0, 2, 1, 
+            0, 3, 2,
 
-            // Sides (four triangles meeting at the apex)
-            0, 1, 4, // Front face
-            1, 2, 4, // Right face
-            2, 3, 4, // Back face
-            3, 0, 4  // Left face
+            0, 1, 4, 
+            1, 2, 4, 
+            2, 3, 4, 
+            3, 0, 4  
         };
 
         mesh.triangles = triangles;
 
-        // Recalculate normals for proper lighting/shading
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
         mesh.Optimize();
